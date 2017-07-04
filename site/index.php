feeab37c062ad09895c99d8e69eab8aa5badeac5
Feb Dao <hien.dao@ffwagency.com>
2017-07-04 11:40:49 +0700

    UPdate structure

--- site/index.php
+++ site/index.php
@@ -16,77 +16,8 @@
 	<meta name="description" content="Góc nhỏ để yêu thương | Truyện ngắn | Blog tình yêu">
 	<meta name="keywords" content="Góc nhỏ để yêu thương | Truyện ngắn | Blog tình yêu">
 	<meta property="fb:pages" content="421944548010384" />
-	<style type="text/css">
-		body {
-			margin: 0;
-			text-align: center;
-		}
+	<link rel="stylesheet" href="style.css">
 
-		iframe {
-			vertical-align: top;
-			max-width: 100%;
-			margin: 5px;
-		}
-
-		img {
-			max-width: 100%;
-			height: auto;
-		}
-
-		.content,
-		.sidebar {
-			padding: 10px 5px;
-			display: inline-block;
-			vertical-align: top;
-			max-width: 100%;
-		}
-
-		.content {
-			max-width: 740px;
-		}
-
-		.sidebar {
-			width: 350px;
-			background: #eee;
-			margin-top: 17px;
-			text-align: left;
-		}
-
-		.sidebar h3 {
-			padding: 0 15px;
-			color: #456;
-			margin-bottom: 0px;
-		}
-
-		.footer {
-			background: #456;
-			padding: 10px;
-		}
-
-		.footer a {
-			color: #FFF;
-			text-decoration: none;
-		}
-
-		.list-post {
-	    list-style: none;
-	    padding-left: 0;
-	    margin-top: 5px;
-		}
-
-		.list-post a {
-	    color: #890;
-	    text-decoration: none;
-	    padding: 3px 15px;
-	    display: block;
-	    border-bottom: 1px dotted #ddd;
-		}
-
-		.list-post a:hover {
-			color: #456;
-		}
-
-	</style>
 </head>
 <body>
 	<div class="banner desktop-only">
@@ -96,49 +27,50 @@
 	</div>
 
 	<div class="main-content">
-		<div class="sidebar">
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgocyeuthuong.net%2F&tabs&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=292300460886432" width="340" height="70" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+		<div class="grid js--grid">
+			<div class="grid__item list--post sidebar">
+				<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgocyeuthuong.net%2F&tabs&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=292300460886432" width="340" height="70" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 
-			<h3>10 bài ngẫu nhiên</h3>
-			<ul class="list-post">
+				<h3>10 bài ngẫu nhiên</h3>
+				<ul class="list-post">
+				<?php
+					$random_keys = array_rand($data['ids'], 10);
+
+					// Random post
+					for( $i = 0; $i<10; $i++ ) {
+	            print '<li><a href="https://www.facebook.com/gocyeuthuong.net/posts/'.$data['ids'][$random_keys[$i]]["id"].'" target="_blank">⊹ '.$data['ids'][$random_keys[$i]]["title"].'</a></li>';
+	         }
+
+					// Get all post
+					// foreach ($random_data['ids'] as $item) {
+					// 	print '<li><a href="https://www.facebook.com/gocyeuthuong.net/posts/'.$item["id"].'">⊹ '.$item["title"].'</a></li>';
+					// }
+				?>
+				</ul>
+			</div>
+
 			<?php
-				$random_keys = array_rand($data['ids'], 10);
-
-				// Random post
-				for( $i = 0; $i<10; $i++ ) {
-            print '<li><a href="https://www.facebook.com/gocyeuthuong.net/posts/'.$data['ids'][$random_keys[$i]]["id"].'">⊹ '.$data['ids'][$random_keys[$i]]["title"].'</a></li>';
-         }
-
-				// Get all post
-				// foreach ($random_data['ids'] as $item) {
-				// 	print '<li><a href="https://www.facebook.com/gocyeuthuong.net/posts/'.$item["id"].'">⊹ '.$item["title"].'</a></li>';
-				// }
+				foreach ($data['ids'] as $item) {
+					print '<div class="grid__item">';
+					print $item["iframe"];
+					print '</div>';
+				}
 			?>
-			</ul>
-		</div>
-
-		<div class="content">
-		<?php
-			foreach ($data['ids'] as $item) {
-				if($item["type"] == "video") {
-					print '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fgocyeuthuong.net%2Fvideos%2F'.$item["id"].'%2F&width=350&show_text=true&appId=292300460886432&height=344" width="350" height="344" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
-				}
-				else if($item["type"] == "pic") {
-					print '<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fgocyeuthuong.net%2Fposts%2F'.$item["id"].'%3A0&width=350" width="350" height="790" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
-				}
-				else {
-					print '<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fgocyeuthuong.net%2Fposts%2F'.$item["id"].'%3A0&width=350" width="350" height="529" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>';
-				}
-
-			}
-		?>
-		</div>
 
 	</div>
 
 	<div class="footer">
 		<a href="http://gocyeuthuong.net" title="Gocyeuthuong">GocYeuThuong.Net © 2017 </a>
 	</div>
-	
+
+	<script
+	  src="https://code.jquery.com/jquery-1.12.4.min.js"
+	  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+	  crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-migrate-3.0.0.min.js" integrity="sha256-JklDYODbg0X+8sPiKkcFURb5z7RvlNMIaE3RA2z97vw=" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+
+	<script type="text/javascript" src="script.js"></script>
+
 </body>
 </html>
---
+++ site/script.js
@@ -0,0 +1,9 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  // $('.grid').masonry({
+  //   // options
+  //   itemSelector: '.grid__item',
+  //   isAnimated: !Modernizr.csstransitions,
+  //   resize: true
+  // });
+});
